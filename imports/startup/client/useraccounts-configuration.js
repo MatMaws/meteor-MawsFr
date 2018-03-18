13d65cce23a51e50f8bf7442510bfea7352b2bb6
Mustapha Nezzari <mnezzari@gmail.com>
2018-03-18 10:25:08 +0100

    t

--- imports/startup/client/useraccounts-configuration.js
+++ imports/startup/client/useraccounts-configuration.js
@@ -1,1 +1,6 @@
+import { Accounts } from 'meteor/accounts-base';
 // TODO : Injecter le code pour l'authent
+
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_ONLY', // un pseudo au lieu d'un email pour l'authentification
+});
