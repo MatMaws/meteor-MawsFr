9acb952bdbaac9abbb6172b438a3be44c7466939
Mustapha Nezzari <mnezzari@gmail.com>
2018-03-18 10:00:59 +0100

    t

--- imports/ui/components/navbar/index.js
+++ imports/ui/components/navbar/index.js
@@ -4,3 +4,9 @@ import './navbar.css';
 import { Template } from 'meteor/templating';
 
 // TODO: Ajouter les helpers
+Template.navbar.helpers({
+  dollarWallet() {
+    return 9876;
+  },
+  currency: 'dollars',
+});
