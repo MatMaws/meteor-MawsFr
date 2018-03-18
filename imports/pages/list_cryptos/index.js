fc5e17635a92c388d4d0627344fe5f55a1bb591f
Mustapha Nezzari <mnezzari@gmail.com>
2018-03-18 10:51:22 +0100

    t

--- imports/pages/list_cryptos/index.js
+++
@@ -1,7 +0,0 @@
-import './list_crypto.html';
-import './list_crypto.css';
-
-import { Crypto } from '../../../api/crytocurrency/crytocurrency.js';
-import { Wallets } from '../../../api/wallets/wallets.js';
-
-// TODO : Insérer les souscriptions et les helpers
--- imports/ui/pages/list_cryptos/index.js
+++ imports/ui/pages/list_cryptos/index.js
@@ -3,24 +3,3 @@ import './list_crypto.css';
 
 import { Crypto } from '../../../api/crytocurrency/crytocurrency.js';
 import { Wallets } from '../../../api/wallets/wallets.js';
-
-Template.list_crypto.onCreated(function() {
-  this.autorun(() => {
-    this.subscribe('crypto');
-    this.subscribe('Wallets');
-  });
-});
-
-Template.list_crypto.helpers({
-  cryptos() {
-    return Crypto.find();
-  },
-});
-
-Template.crypto.helpers({
-  inWallet() {
-    return Wallets.findOne({
-      $and: [{ code: this.code }, { owner: Meteor.userId() }],
-    }).nbCoins;
-  },
-});
